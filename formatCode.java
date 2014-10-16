8d454033760bdd2289f4c85ecc1e257594634fa4
Habaroff <habaroff55@yandex.ru>
2014-10-16 12:46:41 +0700

    new line

--- formatCode.java
+++ formatCode.java
@@ -1,5 +1,20 @@
-class formatCode{
-	public static void main(String[]args){
-		System.out.println("llaskdfl");
-	}	
+import java.io.*;
+import java.util.*;
+class formatCode
+	{
+	public static void main(String[]args)
+		throws IOException
+	{
+		System.out.println("Enter name of file, which will change");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		BufferedReader br = new BufferedReader(new FileReader(name));
+		String code = br.readLine();
+		code = format(code);
+		System.out.println(code);
+	}
+	public static String format(String code){
+		code = code.replaceAll(";","; \n");
+		return code;
+	}
 }
